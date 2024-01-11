0d13e24788a7c5cdfe4be8fbc40cbd5c9072c4b0
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-11 16:50:08 +0300

    5.3 Заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages: " + ages.length);
         System.out.println("Размер массива surname: " + surname.length);
         System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Maria Ivanova";
+        names[2] = "Ivan Maltsev";
+        names[3] = "Maria Maltseva";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
