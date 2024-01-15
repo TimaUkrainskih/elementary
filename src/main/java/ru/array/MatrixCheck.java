6074eeb6b883d35e9515b65aaca58f4006f71d4c
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-15 12:52:01 +0300

    5.30 Выирышные комбинации в сокобан

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -34,10 +34,9 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < 5; i++) {
-            if (board[i][i] == 'X') {
-                if (monoVertical(board, i) || monoHorizontal(board, i)) {
-                    result = true;
-                }
+            if (board[i][i] == 'X' && (monoVertical(board, i) || monoHorizontal(board, i))) {
+               result = true;
+               break;
             }
         }
         return result;
